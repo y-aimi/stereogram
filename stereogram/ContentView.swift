de51f479063c08930a9d45278e6534683ef1d016
yoshiki <yoshiki0885@gmail.com>
2020-09-27 16:24:00 +0900

    Initial Commit

---
+++ stereogram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  stereogram
+//
+//  Created by 相見佳輝 on 2020/09/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ stereogram/stereogramApp.swift
@@ -0,0 +1,17 @@
+//
+//  stereogramApp.swift
+//  stereogram
+//
+//  Created by 相見佳輝 on 2020/09/27.
+//
+
+import SwiftUI
+
+@main
+struct stereogramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
